4406a791e5ef9a49d3368bf18998bc73f701ead9
samcasmmm <sameerbagwan132@gmail.com>
2022-02-05 18:51:50 +0530

    Third commit

--- app/src/main/java/com/example/objectdetect_v1/MainActivity.java
+++ app/src/main/java/com/example/objectdetect_v1/MainActivity.java
@@ -4,10 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import org.opencv.android.OpenCVLoader;
+
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d("MainActivity ","OpenCV is Loaded");
+        }
+        else {
+            Log.d("MainActivity ","OpenCV is Not Loaded");
+        }
+    }
+
     Button RTODetectionBtn,HandSignBtn,EditProfileBtn;
 
     @Override
@@ -25,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),UpdateProfile.class));
+            }
+        });
+
+        RTODetectionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }
         });
     }
