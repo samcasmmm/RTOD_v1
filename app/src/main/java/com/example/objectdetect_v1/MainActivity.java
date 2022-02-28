35bc25ce3673460a35b81e0546a794c9aa49b195
samcasmmm <sameerbagwan132@gmail.com>
2022-02-28 11:58:59 +0530

    9th Major UI Update(Redesigned UI)

--- app/src/main/java/com/example/objectdetect_v1/MainActivity.java
+++ app/src/main/java/com/example/objectdetect_v1/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.objectdetect_v1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import com.example.objectdetect_v1.chatbot.*;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView chatBotImg;
 
     static {
         if(OpenCVLoader.initDebug()){
@@ -31,7 +34,15 @@ public class MainActivity extends AppCompatActivity {
         RTODetectionBtn = findViewById(R.id.ObjectDetectionBTN);
         HandSignBtn = findViewById(R.id.handSignBTN);
         EditProfileBtn = findViewById(R.id.EditProfileBTN);
+        chatBotImg = findViewById(R.id.id_chatBot);
 
+
+        chatBotImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, com.example.objectdetect_v1.chatbot.MainActivity.class));
+            }
+        });
 
 
         EditProfileBtn.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/objectdetect_v1/chatbot/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.objectdetect_v1.chatbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.objectdetect_v1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbot_main);
+    }
+}
